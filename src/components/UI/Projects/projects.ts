a433fec3179d9c3d3ed5232f1a1941e0a5b9e826
Lorenzo Larghi <elel.develop@gmail.com>
2025-06-26 20:23:08 +0100

    Split into threesss (#17)
    * 2025 udpate
    
    * feat: fixes on 2025
    
    * feat: added revamp of the site
    
    * feat: upgrades
    
    * feat: updated resume
    
    * feat: updated readme

--- src/components/UI/Projects/projects.ts
+++ src/components/UI/Projects/projects.ts
@@ -110,7 +110,7 @@ export const projectsList: Project[] = [
   {
     name: "SOLAR Consumer Assistant",
     description:
-      "AI product lead for the creation of the first LLM agentic Conversional Consumer Assistant in Denmark for the support of customers in their everyday shopping needs. Assistant spans across 4 countries with multilanguage and multitools support. Coded the main AI functionalities and mantainer for the evaluation and observability pipelines.",
+      "AI tech lead for the creation of the first LLM agentic Conversional Consumer Assistant in Denmark for the support of customers in their everyday shopping needs. Assistant spans across 4 countries with multilanguage and multitools support. Coded the main AI functionalities and mantainer for the evaluation and observability pipelines.",
     stack: ["Azure OpenAI", "GPT", "Word Embeddings", "OpenAI Assistants API", "Datadog"],
     githubLink: "https://www.solar.dk/nyheder/solar-assistent/",
     liveLink: "https://www.solar.dk/nyheder/solar-assistent/",
--- src/components/UI/Works/worksTypes.tsx
+++ src/components/UI/Works/worksTypes.tsx
@@ -20,20 +20,21 @@ const linkStyle = {
 
 export const works: Work[] = [
   {
-    role: "Backend developer",
+    role: "Backend+AI developer",
     company: "SHAPE",
     link: "https://shape.dk",
     from: "11-2023",
     to: "current",
     description: [
       <p>
-        Developed PubSub services for the real time monitor of energy and fueling transactions.
+        Worked on a different selection of clients, focusing mainly on:
       </p>,
       <p>
-        Increased delivery latency health by 50%, by tweaking EF db calls performance.
+        PubSub services for the real time monitor of energy and fueling transactions,
+         CI/CD pipelines maintainer; DataDog team for better observability.
       </p>,
       <p>
-        CI/CD pipelines maintainer; DataDog team for better observability.
+        AI chatbot implementations, with complex RAG and function tools setup.
       </p>,
     ],
     icon: <Servers />,
@@ -58,25 +59,11 @@ export const works: Work[] = [
     icon: <Servers />,
   },
   {
-    role: "ALEXA Developer",
-    company: "Vocalime",
-    link: "https://www.vocalime.com/",
-    from: "08-2022",
-    to: "10-2022",
-    description: [
-      <p>Developed Alexa skills for an Amazon sub-contractor company.</p>,
-      <p>
-        Tasks: conversational designer, API, Jovo framework contributions...
-      </p>,
-    ],
-    icon: <Chat />,
-  },
-  {
-    role: "Backend Developer",
+    role: "Backend+WEB3 Developer",
     company: "NANOVERSE HQ",
     link: "https://opensea.io/collection/nano-agents",
-    from: "01-2022",
-    to: "07-2022",
+    from: "10-2021",
+    to: "01-2023",
     description: [
       <p>
         Developed the first{" "}
@@ -88,17 +75,39 @@ export const works: Work[] = [
         >
           GPT like
         </a>{" "}
-        customizable virtual assistant (Alexa) in the blockchain ecosystem.
+        customizable virtual assistant in the blockchain ecosystem.
       </p>,
       <p>
         Coded the core API, target of thousands of concurrent users on public
         demo launch.
       </p>,
+      <p>
+        Coded Agents' <a
+          style={linkStyle}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://etherscan.io/address/0x6dcd514e73653fdb8dc46b6e77538bb3069a7e0f#code"
+        >smart contract</a>, pushing to adopt ERC-721A which was a big thing in 2021.
+      </p>,
     ],
     icon: <Servers />,
   },
   {
-    role: "Fullstack Developer",
+    role: "ALEXA Developer",
+    company: "Vocalime",
+    link: "https://www.vocalime.com/",
+    from: "01-2022",
+    to: "03-2022",
+    description: [
+      <p>Developed Alexa skills for an Amazon sub-contractor company.</p>,
+      <p>
+        Tasks: conversational designer, API, Jovo framework contributions...
+      </p>,
+    ],
+    icon: <Chat />,
+  },
+  {
+    role: "Fullstack+AI Developer",
     company: "VirtHuLab",
     link: "https://www.forlilpsi.unifi.it/vp-328-virthulab-virtual-human-dynamics-laboratory.html",
     from: "09-2018",
@@ -109,6 +118,41 @@ export const works: Work[] = [
         Development of web and mobile apps that bring together AI and
         psychology.
       </p>,
+      <p>
+        Contributed to many AI scientific papers, like:{" "}
+        <a
+          style={linkStyle}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://flore.unifi.it/bitstream/2158/1349311/1/conference_101719_ebin.pdf"
+        >
+          wafer-et
+        </a>, one {" "}
+        <a
+          style={linkStyle}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ieeexplore.ieee.org/abstract/document/10584158"
+        >
+          application of it
+        </a>, {" "}
+        <a
+          style={linkStyle}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=10477992"
+        >
+          fer comparisons
+        </a>, {" "}
+        <a
+          style={linkStyle}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://flore.unifi.it/bitstream/2158/1349314/1/Eye-tracking_correlates_of_the_Implicit_Association_Test%20%281%29.pdf"
+        >
+          iat
+        </a>.
+      </p>
     ],
     icon: <BatteryFull />,
   },
