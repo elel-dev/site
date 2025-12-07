98a947e63b42eac2350d9f8d57e8296062943ce5
Lorenzo Larghi <elel.develop@gmail.com>
2025-12-07 20:08:56 +0000

    Cleaning up and updating dependencies  (#18)
    * feat: update dependencies and integrate updated astro-icon component
    
    * refactor: streamline index.astro layout and improve styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
   vite: {
     ssr: {
       external: ["svgo"],
